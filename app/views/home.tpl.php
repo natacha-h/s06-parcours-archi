8cd6fbe98685b7628ec7de90124eb59017362ff0
natacha-h <natacha.heubes@hotmail.fr>
2019-03-13 11:53:30 +0100

    encore un peu de commentaires

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,8 +1,8 @@
 
 <!-- Personnage -->
 
-<table class="table">
-    <thead>
+<table class="table table-bordered">
+    <thead class="thead-dark">
         <tr>
         <th scope="col">Image</th>
         <th scope="col">Nom</th>
@@ -11,6 +11,7 @@
         </tr>
     </thead>
     <tbody>
+    <!-- on boucle sur le tableau $characters our afficher les lignes du tableau -->
         <?php foreach ($characters as $character):?>
             <tr>
             <td><img class="img-fluid max-width: 8%;"src="<?=$baseUrl . '/docs/images/' . $character['picture'] ?>"></td>
